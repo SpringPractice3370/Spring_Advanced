0024c48413daa4fbbe942cb3e05bb3a68689fe99
sectionr0 <sectionr0@gmail.com>
2022-08-16 23:56:09 +0900

    template method study

--- advanced/src/test/java/com/example/advanced/trace/threadlocal/code/ThreadLocalService.java
+++ advanced/src/test/java/com/example/advanced/trace/threadlocal/code/ThreadLocalService.java
@@ -1,6 +1,10 @@
 package com.example.advanced.trace.threadlocal.code;
 
+import com.example.advanced.trace.template.code.AbstractTemplate;
+import com.example.advanced.trace.template.code.SubClassLogin1;
+import com.example.advanced.trace.template.code.SubClassLogin2;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class ThreadLocalService {
@@ -21,5 +25,39 @@ public class ThreadLocalService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogin1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogin2();
+        template2.execute();
+    }
+
+    /**
+     * 템플릿 메서드 패턴, 익명 내부 클래스 사용 */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
     }
 }
